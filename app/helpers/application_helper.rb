c99896ae377b089d6641251be284ce1d4b3779d8
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2021-07-09 14:22:00 -0300

    :sparkles: add rails_environment helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,14 @@ module ApplicationHelper
   def date_us_to_br(date_us)
     date_us.strftime("%d/%m/%Y")
   end
+
+  def rails_environment
+    if Rails.env.development?
+      "Desenvolvimento"
+    elsif Rails.env.production?
+      "Produção"
+    else
+      "Teste"
+    end
+  end
 end
