8f0ac06381209f49de14d17c226fc5d1c45983e3
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2021-07-10 10:44:23 -0300

    :sparkles: query params to requests

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,12 @@
 class WelcomeController < ApplicationController
   def index
     # passar dados do controller para a view
-    @name = "Maiqui Pirolli Tomé"
+    # use uma variável de instância: @something
+
+    # query params (Via GET):
+    # http://localhost:3000/?name=Mike&course=Ruby on Rails
+
+    @my_name = params[:name] || "Maiqui Pirolli Tomé"
+    @course = params[:course]
   end
 end
